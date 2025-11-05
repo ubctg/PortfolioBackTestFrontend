c8940305138b13e671b37899280f6f0680e0af2a
joseph784 <64226382+joseph784@users.noreply.github.com>
2025-11-04 22:50:39 -0800

    Add selectedMonth prop to ReturnsModal component

--- src/Components/ReturnsModal.jsx
+++ src/Components/ReturnsModal.jsx
@@ -2,27 +2,31 @@ import { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import GranularLineGraph from "./GranularLineGraph";
 
-export default function Contact({showModal, onClose, stockData, allocations, balance}){
+export default function Contact({showModal, onClose, stockData, allocations, balance, selectedMonth}){
     const [returns, setReturns] = useState()
     const [showBL, setShowBL] = useState(true)
 
+
     useEffect(() => {
       let blReturns = {};
+      if (!stockData || typeof stockData !== 'object' || !allocations || typeof allocations !== 'object') {
+        setReturns({});
+        return;
+      }
       for (const key in allocations) {
         if (allocations.hasOwnProperty(key)) { 
           const weight = allocations[key].Weight; 
           const ticker = allocations[key].Ticker; 
-          if (stockData[ticker]) {
+          if (stockData && stockData[ticker] && Array.isArray(stockData[ticker])) {
             const temp = stockData[ticker].map(value => weight*balance + (value * weight * balance)); 
             blReturns[ticker] = temp
           }
         }
       }
       setReturns(blReturns)
-    }, [])
+    }, [stockData, allocations, balance])
 
     return(
-    
         <> 
     <Modal
         show={showModal}
@@ -32,14 +36,14 @@ export default function Contact({showModal, onClose, stockData, allocations, bal
     >
       <Modal.Header>
         <Modal.Title style={{color: 'black'}}>
-          {showBL? "Stock Returns" : "Stock Balance"}
+          {showBL? "Raw Stock Returns" : "Black-Litterman Balance"}
         </Modal.Title>
-        
       </Modal.Header>
-      <Modal.Body><GranularLineGraph stockData={showBL? stockData : returns} />
+      <Modal.Body>
+        <GranularLineGraph stockData={showBL? stockData : returns} selectedMonth={selectedMonth}/>
       </Modal.Body> 
       <Modal.Footer>
-        <Button onClick={() => setShowBL(!showBL)} variant="dark">Black Litterman Evaluation</Button>
+  <Button onClick={() => setShowBL(!showBL)} variant="dark">{!showBL ? "Stock Returns" : "Black Litterman Evaluation"}</Button>
         <Button onClick={onClose} variant="dark">Close</Button>
         
       </Modal.Footer>
